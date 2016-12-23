448919e29a3afb74736c786b8a8213be9f428e77
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2016-12-23 13:27:57 +0100

    UPDATE polyfills.ts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,14 +2,12 @@ import 'core-js/es6';
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 
-import 'ts-helpers';
-
 if (process.env.ENV === 'production') {
     // Production
 } else
 {
     // Development
-    Error.stackTraceLimit = Infinity;
-
+    Error['stackTraceLimit'] = Infinity;
+    
     require('zone.js/dist/long-stack-trace-zone');
 }
