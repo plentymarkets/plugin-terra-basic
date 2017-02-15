a68018187b2780ac2aa226ef7b4d344cc846419a
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2017-02-15 16:21:36 +0100

    FIX build config & package.json

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,7 +2,6 @@ const helpers = require('./helpers');
 const webpack = require('webpack');
 const DefinePlugin = require('webpack/lib/DefinePlugin');
 const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
-const WebpackMd5HashPlugin = require('webpack-md5-hash');
 const CompressionPlugin = require('compression-webpack-plugin');
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
@@ -30,15 +29,13 @@ module.exports = function (env) {
 
         plugins: [
 
-            new WebpackMd5HashPlugin(),
-
             new DefinePlugin({
                 'ENV': JSON.stringify(METADATA.ENV),
                 'HMR': METADATA.HMR,
                 'process.env': {
                     'ENV': JSON.stringify(METADATA.ENV),
                     'NODE_ENV': JSON.stringify(METADATA.ENV),
-                    'HMR': METADATA.HMR,
+                    'HMR': METADATA.HMR
                 }
             }),
 
@@ -57,6 +54,12 @@ module.exports = function (env) {
             new CompressionPlugin({
                 regExp: /\.css$|\.html$|\.js$|\.map$/,
                 threshold: 2 * 1024
+            }),
+
+            new webpack.LoaderOptionsPlugin({
+                htmlLoader: {
+                    minimize: false // workaround for ng2
+                }
             })
 
         ]
