afcc9a0fb7a09bc66db7c97965e17c90657c4674
Daniel Trauf <daniel.trauf@plentymarkets.com>
2017-07-28 15:47:48 +0200

    added Forms Module for ngModel

--- src/app/plugin-terra-basic.module.ts
+++ src/app/plugin-terra-basic.module.ts
@@ -6,11 +6,13 @@ import { TerraComponentsModule } from '@plentymarkets/terra-components/app/terra
 import { HttpModule } from '@angular/http';
 import { TestComponent } from './test/test.component';
 import { TranslationModule } from 'angular-l10n';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
               imports:      [
                   BrowserModule,
                   HttpModule,
+                  FormsModule,
                   TranslationModule.forRoot(),
                   TerraComponentsModule.forRoot()
               ],
