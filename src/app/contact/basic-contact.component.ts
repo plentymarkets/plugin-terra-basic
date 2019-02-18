6ee37dfc11b4f29ddd1fc53965a42b3184ce5520
Tim Wieder <tim.wieder@plentymarkets.com>
2019-02-18 10:00:35 +0100

    - schulungs stuff

--- src/app/contact/basic-contact.component.ts
+++ src/app/contact/basic-contact.component.ts
@@ -8,6 +8,7 @@ import {
 import { BasicContactService } from './basic-contact.service';
 import { ContactInterface } from './contact.interface';
 import { TerraPagerInterface } from '@plentymarkets/terra-components';
+import { isNullOrUndefined } from 'util';
 
 @Component({
     selector: 'basic-contact',
@@ -44,8 +45,82 @@ export class BasicContactComponent implements OnInit
     {
         this.basicContactService.getContactById().subscribe((contacts:TerraPagerInterface<ContactInterface>) =>
         {
+            console.log(contacts);
+
+            contacts.entries.forEach((contact:ContactInterface) =>
+            {
+                if(contact.lastName === 'Griesel')
+                {
+                    console.log(contact);
+                    console.log('_____________________');
+                }
+            });
+
+            let contact:ContactInterface = contacts.entries.find((contactToFind:ContactInterface) =>
+            {
+                return contactToFind.lastName === 'Waits';
+            });
+
+            console.log(contact);
+            console.log('_____________________');
+
+            let contactsToBeEdited:ContactInterface = contacts.entries.filter((contactToFilter:ContactInterface) =>
+            {
+                return contactToFilter.firstName.startsWith('J');
+            })
+
+
+
+
+                                                              .filter((contactToFilterFilter:ContactInterface) =>
+            {
+                return contactToFilterFilter.firstName.startsWith('Jo');
+            }).find((contactToFilterFilterFind:ContactInterface) =>
+            {
+                return contactToFilterFilterFind.firstName === 'Joseph';
+            });
+
+            console.log(contactsToBeEdited);
+            console.log('_____________________');
+
+            let firstEntry:ContactInterface = contacts.entries[200];
+
+            if(!isNullOrUndefined(firstEntry))
+            {
+                console.log('Doch nicht');
+            }
+
+            console.log('This should be undefined');
+            console.log(firstEntry);
+
+            let changedContacts:Array<ContactInterface> = contacts.entries.map((mappedContact:ContactInterface) =>
+            {
+                if(mappedContact.firstName.startsWith('J'))
+                {
+                    mappedContact.lastName = 'Wieder';
+                }
+
+                return mappedContact;
+            });
+
+            console.log('There should be some agains now');
+            console.log(changedContacts);
+
+            const nonUniqueArray = ['foo', 'foo', 'bar'];
+
+            console.log(nonUniqueArray);
+
+            console.log(nonUniqueArray.filter((entry:string, index:number, selfArray:Array<string>) => {
+                return selfArray.indexOf(entry) === index;
+            }));
+
+            const numbers: Array<number> = [1, 2, 3, 4, 5];
+
+            console.log(numbers);
 
         });
     }
-    
+
+
+
 }
