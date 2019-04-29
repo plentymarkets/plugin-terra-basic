e6fad29da2719b02fed1aac112e9f208022020e8
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-04-29 13:57:55 +0200

    wip

--- src/app/contact-search/contact-search.component.ts
+++ src/app/contact-search/contact-search.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    OnInit
+} from '@angular/core';
 import { BasicContactService } from '../contact/basic-contact.service';
 import {
     FormControl,
     FormGroup
 } from '@angular/forms';
+import {
+    debounceTime,
+    distinctUntilKeyChanged,
+    map,
+    tap,
+    filter,
+    switchMap,
+    catchError
+} from 'rxjs/operators';
+
+import { zip } from 'rxjs/operators/zip';
+import { combineLatest } from 'rxjs/operators/combineLatest';
+import { of } from 'rxjs/observable/of';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector:    'terra-contact-search',
     templateUrl: './contact-search.component.html',
     styleUrls:   ['./contact-search.component.scss']
 })
-export class ContactSearchComponent
+export class ContactSearchComponent implements OnInit
 {
-
     private form:FormGroup;
 
     constructor(private contactService:BasicContactService)
@@ -20,5 +36,29 @@ export class ContactSearchComponent
         this.form = new FormGroup({
             name: new FormControl('')
         });
+    }
+
+    public ngOnInit():void
+    {
+        let changes$:Observable<any> = this.form.valueChanges.pipe(debounceTime(400));
+        let filtered$:Observable<string> = this.form.valueChanges.pipe(
+            map((changes:any) => changes.name),
+            filter((name:string) => name.length > 3),
+        );
+
+        this.form.valueChanges.pipe(
+            debounceTime(400),
+            distinctUntilKeyChanged('name'),
+            tap((changes:any) => console.log('tap', changes)),
+            map((changes:any) => changes.name),
+            filter((name:string) => name.length > 3),
+            switchMap((name:string) => this.contactService.getContactById(name.length))
+        );
+
+        let array$:Array<Observable<any>> = [changes$, filtered$];
+
+        Observable.zip(array$, (zipped:[any, string]) => zipped[0]).subscribe((zipped:any) => console.log('zipped', zipped));
+
+        Observable.combineLatest(changes$, filtered$, () => ).subscribe((combined:any) => console.log('combined', combined))
     }
 }
