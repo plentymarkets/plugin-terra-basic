56e45d26ede265d0d13f46f19ade6857b24318e9
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2019-06-28 16:02:51 +0200

    removed unused files clean up package.json

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { environment } from './environments/environment';
-import { PluginTerraBasicModule } from './app';
+import { PluginTerraBasicModule } from './app/plugin-terra-basic.module';
 
 if(environment.production)
 {
     enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(PluginTerraBasicModule)
-                        .catch((err:any) => console.error(err));
+platformBrowserDynamic().bootstrapModule(PluginTerraBasicModule).catch((err:any) => console.error(err));
