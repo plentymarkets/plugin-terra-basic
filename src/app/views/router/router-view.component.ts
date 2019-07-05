0810dd5bb46a36b5a5bc5bd67b9cc89521fdb69e
Michael Frank <michael.frank@plentymarkets.com>
2019-07-05 10:36:00 +0200

    fix path to template url

--- src/app/views/router/router-view.component.ts
+++ src/app/views/router/router-view.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'ptb-router-view',
-    templateUrl: ('./router-view.component.html')
+    templateUrl: './router-view.component.html'
 })
 export class RouterViewComponent
 {
