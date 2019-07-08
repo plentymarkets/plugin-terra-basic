fad0d8ff1cae8c235ebb917b40326f1dd9f8ce14
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-07-08 15:01:10 +0200

    fix errors in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
 webpack = require('webpack');
 
 module.exports = function(config, options) {
-    var env = process.env.ENV = process.env.NODE_ENV = options.fileReplacements.length > 0 ? 'production' : 'development';
+    var env = process.env.ENV = process.env.NODE_ENV = options && options.fileReplacements.length > 0 ? 'production' : 'development';
 
     console.log('------- Webpack Configuration -------');
     console.log('\'process.env.ENV\' is set to: ' + env);
     console.log('-------------------------------------');
 
-    config.plugins.push(
-        new webpack.DefinePlugin({
+    if(config && config.plugins) {
+        const definePlugin = new webpack.DefinePlugin({
             'process.env': {
                 'ENV': JSON.stringify(env)
             }
-        })
-    );
+        });
+        if (config.plugins) {
+            config.plugins.push(definePlugin);
+        } else {
+            config.plugins = [definePlugin];
+        }
+    }
 
     return config;
 };
