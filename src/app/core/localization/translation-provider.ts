8805926536b2892a99357d782766cae846d3fae1
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-07-08 10:58:48 +0200

    Create a custom TranslationProvider to get a clean instance of the HttpClient which "dispatches the requests directly to the backend without going through the interceptor chain"

---
+++ src/app/core/localization/translation-provider.ts
@@ -0,0 +1,30 @@
+import {
+    Inject,
+    Injectable
+} from '@angular/core';
+import {
+    Caching,
+    L10N_CONFIG,
+    L10nConfigRef,
+    L10nTranslationProvider
+} from 'angular-l10n';
+import {
+    HttpBackend,
+    HttpClient
+} from '@angular/common/http';
+
+/**
+ * @description A custom TranslationProvider alà angular-l10n which creates a clean instance of the HttpClient.
+ * The HttpClient instance then "dispatches the requests directly to the backend without going through the interceptor chain" .
+ * See https://angular.io/api/common/http/HttpBackend#description for details.
+ */
+@Injectable()
+export class TranslationProvider extends L10nTranslationProvider
+{
+    constructor(private httpBackend:HttpBackend,
+                @Inject(L10N_CONFIG) config:L10nConfigRef,
+                caching:Caching)
+    {
+        super(new HttpClient(httpBackend), config, caching);
+    }
+}
