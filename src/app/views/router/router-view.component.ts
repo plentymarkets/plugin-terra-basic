1795a77c3424682a74951acba0d3f004f1a229f4
Kevin Fink <kevin.fink@plentymarkets.com>
2019-07-12 08:19:09 +0200

    loading scss file to fix overflow issue

--- src/app/views/router/router-view.component.ts
+++ src/app/views/router/router-view.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'ptb-router-view',
-    templateUrl: './router-view.component.html'
+    templateUrl: './router-view.component.html',
+    styleUrls:   ['./router-view.component.scss']
 })
 export class RouterViewComponent
 {
