d4b0348a50afcab671c4d4b0d8ccbde170083d32
ChristianMoll <christian.moll@plentymarkets.com>
2019-08-08 16:20:57 +0200

    remove import

--- src/app/plugin-terra-basic.module.ts
+++ src/app/plugin-terra-basic.module.ts
@@ -16,7 +16,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import {
     appRoutingProviders,
-    routing
 } from './plugin-terra-basic.routing';
 import {
     httpInterceptorProviders,
