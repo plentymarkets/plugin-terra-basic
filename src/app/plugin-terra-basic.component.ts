f064d4c398560c806714b4b50176ef62630d8b1d
Michael Frank <michael.frank@plentymarkets.com>
2019-09-12 09:19:16 +0200

    Update plugin-terra-basic.component.ts

--- src/app/plugin-terra-basic.component.ts
+++ src/app/plugin-terra-basic.component.ts
@@ -13,6 +13,7 @@ export class PluginTerraBasicComponent implements OnInit
     public readonly nuts:Array<string> = ['Hazel', 'Almond'];
     public readonly fruits:Array<string> = ['Banana', 'Orange','Cherry'];
     public readonly strings:Array<string> = this.fruits.concat(this.nuts);
+    public readonly numbers:Array<number> = [22, 8, 19, 44, 35, 2];
 
     private readonly banana:Object = {
         color: 'Yellow',
@@ -27,7 +28,10 @@ export class PluginTerraBasicComponent implements OnInit
         this.objectEntries(this.banana);
     }
 
-    private readonly numbers:Array<number> = [22, 8, 19, 44, 35, 2];
+    public allIndices(array:Array<string>):Array<string>
+    {
+        return null;
+    }
 
     public allFruits(array:Array<string>):Array<string>
     {
@@ -35,11 +39,6 @@ export class PluginTerraBasicComponent implements OnInit
     }
 
     public fruitWith(array:Array<string>, letter:string):Array<string>
-    {
-        return null;
-    }
-
-    public allIndices(array:Array<string>):Array<string>
     {
         return null;
     }
