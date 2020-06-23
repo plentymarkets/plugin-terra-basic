806ea65888622575b9f579beea5a9a8780610690
Michael Frank <m_frank84@outlook.de>
2020-06-23 12:12:05 +0200

    Update plugin-terra-basic.component.ts

--- src/app/plugin-terra-basic.component.ts
+++ src/app/plugin-terra-basic.component.ts
@@ -14,7 +14,8 @@ export class PluginTerraBasicComponent implements OnInit
 {
     private readonly domain:string = 'http://master.login.plentymarkets.com';
     /*tslint:disable-next-line:max-line-length*/
-    private readonly accessToken:string = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMThmZmViMDIxMWI0MGRjZThlNjEwYTE2OWJiZjMyYmZjZWVjZWVmMWJlOTI3ZGY0MWVkNDQ2MjFhNjQwYzlkZDA2YTgxOGM2YWNjNTc2NGQiLCJpYXQiOjE1OTI0NTk4NDcsIm5iZiI6MTU5MjQ1OTg0NywiZXhwIjoxNTkyNTQ2MjQ3LCJzdWIiOiIzIiwic2NvcGVzIjpbIioiXX0.qDvrMDq9vT2b68V5LwzO2dVC-cknsF9prG5v2a43N7fw5tzmB-XCxh28Z_oaiGI932X8fhQQaw-WuW3YsBAE63qB0zWDcKjKHjCb8TkXiYNwiMC2KNp5PNxoOaHaeG02lhT5n9XJU_zGIf5VALe5ewvM-V05ddBn8RB_BLYt3PNqQnlHBLe2pyVJHH251fjLUxNS4APu9pGQZDMRofhjEnvy0HcyaOHyPlXN9HH3o4kpfsKOHUncVPzBzgdNG6HJ-s8b5t0nXcHL-y2Lvd-zJHP61tHUFREbtkIc4RHCefk_vblRecrsG8I314r0oNxfwhSy2Uvnjj0y4vvZOwsrnjq6-BqdFMqSAQHlZpP-dhwyf1vcdv1QPM-AufxSrq1TXyo-N8G4G2IIA3RvYId4mLRrTAryPGgNuQg_1p0gfhFIORMspOulLlTlHu67dnJZOEhUs30J5TvyA1RAJKUMCzGuKmn5G1yxFTVb0v0dvEZpWl7JRajvNCLLdvxO4PVyXzLY-PjfTeH9KdV600VVvn4XrxWXgEY50jtL-s3Fm-9rgVrCjLVNExBOrwBAZd3RRSZXvDRjVyvm8BTmKVo8Nzo_4edLPYnmYi-HcaLDpmol4GA_p3_0kyM2SsNb0q909bPb9phptnfhDbDGd7elZtdzFDOm5wo5WYWbi5iSeOg';
+    private readonly accessToken:string = '';
+    
     constructor(private httpClient:HttpClient)
     {
     }
