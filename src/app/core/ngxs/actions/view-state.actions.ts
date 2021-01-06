c4055f2d7c57a4702282d666477cd32814304e35
Michael Frank <m_frank84@outlook.de>
2021-01-06 13:20:50 +0100

    update viewState

--- src/app/core/ngxs/actions/view-state.actions.ts
+++ src/app/core/ngxs/actions/view-state.actions.ts
@@ -3,4 +3,14 @@ export namespace ViewStateActions {
         static readonly type: string = '[ViewState] Add View State';
         constructor(public viewKey: string, public viewState: any) {}
     }
+
+    export class Update {
+        static readonly type: string = '[ViewState] Update View State';
+        constructor(public viewKey: string, public viewState: any) {}
+    }
+
+    export class UpdateFilter {
+        static readonly type: string = '[FilterComponent] Update Filter State';
+        constructor(public viewKey: string, public viewState: any) {}
+    }
 }
--- src/app/core/ngxs/state/view-state.state.ts
+++ src/app/core/ngxs/state/view-state.state.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Action, State, StateContext } from '@ngxs/store';
+import { TerraKeyValueInterface } from '@plentymarkets/terra-components';
 import { ViewStateActions } from '../actions/view-state.actions';
 
-@State<Set<any>>({
+@State<TerraKeyValueInterface<any>>({
     name: 'viewStates',
-    defaults: new Set()
+    defaults: {}
 })
 @Injectable()
 export class ViewStateState {
     @Action(ViewStateActions.Add)
-    public addViwState(ctx: StateContext<any>) {}
+    public addViwState(ctx: StateContext<any>, { viewKey, viewState }) {
+        const currentState: Set<any> = ctx.getState();
+        ctx.patchState({ [viewKey]: viewState });
+    }
+
+    @Action([ViewStateActions.Update, ViewStateActions.UpdateFilter])
+    public updateViewState(ctx: StateContext<any>, { viewKey, viewState }) {
+        const currentState: Set<any> = ctx.getState();
+        ctx.patchState({ [viewKey]: { ...currentState[viewKey], ...viewState } });
+    }
 }
